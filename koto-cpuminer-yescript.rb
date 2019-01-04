9dfdbde273244d363488bb4349d795340a4a28b1
Cj-bc <cj-bc@y-modify.org>
2019-01-04 12:19:54 +0900

    Add: checksum

--- koto-cpuminer-yescript.rb
+++ koto-cpuminer-yescript.rb
@@ -5,6 +5,7 @@ class KotoCpuminerYescript < Formula
   homepage "https://github.com/KotoDevelopers/cpuminer-yescrypt"
   version "3.1"
   url "https://github.com/KotoDevelopers/cpuminer-yescrypt/archive/v#{version}.tar.gz"
+  sha256 "f757ae324aacdf8ca281d9ee8de5dc3dc0e94c0efca364ea6757c27260d4da95"
 
   depends_on "autoconf" => [:build, "2.57"]
   depends_on "automake" => [:build, "1.7"]
