63d8e6a3586b0d169f0c049e2a8e20e58fad76cf
Cj-bc <cj-bc@y-modify.org>
2019-01-04 12:17:39 +0900

    Add: formula

---
+++ koto-cpuminer-yescript.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class KotoCpuminerYescript < Formula
+  desc "CPU miner for Koto"
+  homepage "https://github.com/KotoDevelopers/cpuminer-yescrypt"
+  version "3.1"
+  url "https://github.com/KotoDevelopers/cpuminer-yescrypt/archive/V#{version}.tar.gz"
+
+  depends_on "autoconf" => [:build, "2.57"]
+  depends_on "automake" => [:build, "1.7"]
+
+  def install
+    system "./autogen.sh"
+    system "./nomacro.pl"
+    system "./configure CFLAGS='-3'"
+    system "make"
+  end
+end
