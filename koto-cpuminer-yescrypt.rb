e4843274580a45cb225e86742d6253c0f5b29d35
Cj-bc <cj-bc@y-modify.org>
2019-01-05 23:56:02 +0900

    Add: patches for macOS build
    Those patches are comes from:
      * https://qiita.com/s_ktmr/items/a69cbb964a997fcd4d03
      * https://qiita.com/honeniq/items/07ae3d73eb0e8a8fb27f

--- koto-cpuminer-yescrypt.rb
+++ koto-cpuminer-yescrypt.rb
@@ -7,6 +7,7 @@ class KotoCpuminerYescrypt < Formula
   url "https://github.com/KotoDevelopers/cpuminer-yescrypt/archive/v#{version}.tar.gz"
   sha256 "f757ae324aacdf8ca281d9ee8de5dc3dc0e94c0efca364ea6757c27260d4da95"
 
+  patch :DATA
   depends_on "autoconf" => [:build, "2.57"]
   depends_on "automake" => [:build, "1.7"]
 
@@ -17,3 +18,29 @@ class KotoCpuminerYescrypt < Formula
     system "make"
   end
 end
+
+__END__
+diff --git a/Makefile.am b/Makefile.am
+index b314ca7..66c7f74 100644
+--- a/Makefile.am
++++ b/Makefile.am
+@@ -34,3 +34,5 @@ minerd_LDFLAGS  = $(PTHREAD_FLAGS)
+ minerd_LDADD = @LIBCURL@ @JANSSON_LIBS@ @PTHREAD_LIBS@ @WS2_LIBS@
+ minerd_CPPFLAGS = @LIBCURL_CPPFLAGS@
+ 
++# added for mac build; see commit message for details
++ACLOCAL_AMFLAGS = -I m4
+diff --git a/autogen.sh b/autogen.sh
+index 989604a..46cbc2f 100755
+--- a/autogen.sh
++++ b/autogen.sh
+@@ -5,7 +5,8 @@
+ 
+ set -e
+ 
+-aclocal
++# patched; refer commit message for details
++aclocal -I m4
+ autoheader
+ automake --gnu --add-missing --copy
+ autoconf
