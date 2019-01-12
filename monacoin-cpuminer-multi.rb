6051df33fb928bc84d0cb1dc1d263524c36d9ea9
Cj-bc <cj-bc@y-modify.org>
2019-01-12 12:35:06 +0900

    Change: mona-cpu> add sha256

--- monacoin-cpuminer-multi.rb
+++ monacoin-cpuminer-multi.rb
@@ -5,6 +5,7 @@ class MonacoinCpuminerMulti < Formula
   homepage "https://github.com/monacoinproject/cpuminer-multi"
   version "1.2"
   url "https://github.com/monacoinproject/cpuminer-multi/archive/v#{version}-multi.tar.gz"
+  sha256 "da2e9a50cc728c5cdc55e4a4de4d609ff1ca2f636a61b19d08afc124b0829f5f"
 
   depends_on "curl" => :build
   depends_on "zlib" => :build
